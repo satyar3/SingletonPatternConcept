0a5e30d92e8ae43537b2087372a8e6b573eb3cd8
Satyaranjan Muduli <Satyaranjan.Muduli@mea.ey.net>
2018-07-07 11:28:21 +0530

    Uploading singletonpattern

---
+++ src/Practice/DriverInitialization.java
@@ -0,0 +1,17 @@
+package Practice;
+
+public class DriverInitialization {
+
+	/*DriverInitialization dr = null;
+	private webDriver driver;
+	
+	private DriverInitialization()
+	{
+		
+	}
+	public static webDriver openBrowser()
+	{
+		driver = new ChromeDriver();
+	}
+*/
+}
---
+++ src/Practice/SingletonConcept.java
@@ -0,0 +1,50 @@
+package Practice;
+
+public class SingletonConcept {
+	
+	//In OOPS, Singleton class is a class that can have only one object or instance of the class
+	//in case new object or instance created, it'll point to the same object
+	
+	//How to design ?
+	//1.Constructor of that class as Private
+	//2.Write a public static method that has return type object of this singleton class (Lazy Initialization)
+	
+	private static SingletonConcept SingletonConcept_instance = null;
+	
+	public String str;
+	
+	private SingletonConcept()
+	{
+		str = "Test";
+	}
+	
+	public static SingletonConcept getInstance()
+	{
+		if(SingletonConcept_instance == null)//Lazy Initialization
+		{
+			SingletonConcept_instance = new SingletonConcept();
+			System.out.println();
+		}
+		
+		return SingletonConcept_instance;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//SingletonConcept st = new SingletonConcept();
+		SingletonConcept st1 = SingletonConcept.getInstance();
+		SingletonConcept st2 = SingletonConcept.getInstance();
+		SingletonConcept st3 = SingletonConcept.getInstance();
+		
+		st1.str = (st1.str).toUpperCase();
+		System.out.println(st1.str);
+		System.out.println(st2.str);
+		
+		st2.str = (st2.str).toLowerCase();
+		System.out.println(st1.str);
+		System.out.println(st2.str);
+
+	}
+
+}
